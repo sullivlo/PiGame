07e31eb1c49c7f57b1c5c4cd9d9aaf2f1d740fd9
Brendan Nahed <brendannahed@Brendans-MacBook-Pro.local>
2018-03-14 16:21:41 -0400

    Obserable and Observer

---
+++ Observable.py
@@ -0,0 +1,15 @@
+class Observable(object):
+
+        def __init__(self):
+                self.observers = []
+
+        def add_observer(self, observer):
+                if not observer in self.observers:
+                        self.observers.append(observer)
+
+        def remove_observe(self, observer):
+                if observer in self.observers:
+                        self.observers.remove(observer)
+
+        def remove_all_observers(self):
+                self.observers = []
