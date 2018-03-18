695aadab97a76f4b0027f1b92d2a06fcd4151b29
Louis Sullivan <sullivlo@mail.gvsu.edu>
2018-03-18 13:35:18 -0400

    finished NPC

---
+++ NPCS/Werewolves.py
@@ -0,0 +1,27 @@
+
+from random import *
+from NPC import *
+
+class Werewolves(NPC):
+	'''Werewolves are monsters/NPCs whe have a health of 200.
+	as well they had an attack rating between 0 and 40. They are harmed
+	by all weapons, but ChocolateBars or SourStraws.'''
+
+	def __init__(self):
+		super.__init__(self)
+		super.setName(self, 'Zombies')
+		super.setHealth(self, genHealth(self))
+		super.setUnaffCandy(self, genUnaffCandy(self))
+		super.setAltCandy(self, getAltCandy(self))		
+
+	def genHealth(self):
+		return 200
+	def genAttack(self):
+		return random.uniform(0, 40)
+	def genUnaffCandy(self):
+		return ['ChocolateBars', 'SourStraws']
+
+	def genAltCandy(self):
+		return ['']
+
+
