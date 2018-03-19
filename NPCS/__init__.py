2368c4aac59948ecf3f6a770a11172e13ae2dda0
Brendan Nahed <brendannahed@Brendans-MacBook-Pro.local>
2018-03-19 10:21:24 -0400

    NPCS __init__ file

---
+++ NPCS/__init__.py
@@ -0,0 +1,7 @@
+# __init__.py
+__all__ = ['Zombies','Ghouls', 'persons', 'Vampires', 'Werewolves']
+# from Zombies import *
+# from Ghouls import *
+# from persons import *
+# from Vampires import *
+# from Werewolves import *
