5a9be38ad3747728b1c5a7b99bb0be5a65587c13
louissullivan <sullivlo@mail.gvsu.edu>
2018-03-20 20:14:49 -0400

    Added monsters attack

--- Game.py
+++ Game.py
@@ -4,58 +4,70 @@ import Neighborhood
 import Player
 
 class Game(object):
-	'''Game is how the all the objects come and work together.
-	Here contains how everything is built to actually play
-	the game as a user. It brings all the differnet objects
-	together and has them work to complete the game.'''
+    '''Game is how the all the objects come and work together.
+    Here contains how everything is built to actually play
+    the game as a user. It brings all the differnet objects
+    together and has them work to complete the game.'''
 
-	def __init__(self):
+    def __init__(self):
         self.player = Player.Player()
         self.neighborhood = Neighborhood.Neighborhood(randint(2, 10))
         self.turn = True  # player turn = true, monster turn = false
-        				  #	once anyone attacks you switch to what is itsnt now
+                          #    once anyone attacks you switch to what is itsnt now
         self.gameover = False
 
     def move(self):
-    	nextPos = input("\nWhat direction would you like to go in?"
-    		+"\nN for North\nS for South\nE for East\nW for West").upper()
-    	if isValidMove(nextPost):
-    		if (nextPos == 'N'):
-    			self.player.setPosY(self.player.getPosY()-1)
-    		elif (nextPos == 'W'):
-    			self.player.setPosX(self.player.getPosX()-1)
-    		elif (nextPos == 'S'):
-    			self.player.setPosY(self.player.getPosY()+1)
-    		elif (nextPos == 'E'):	
-    			self.player.setPosX(self.player.getPosX()+1)
-    	else:
-    		print("Not a valid move.")
+        nextPos = input("\nWhat direction would you like to go in?"
+            +"\nN for North\nS for South\nE for East\nW for West").upper()
+        if isValidMove(nextPost):
+            if (nextPos == 'N'):
+                self.player.setPosY(self.player.getPosY()-1)
+            elif (nextPos == 'W'):
+                self.player.setPosX(self.player.getPosX()-1)
+            elif (nextPos == 'S'):
+                self.player.setPosY(self.player.getPosY()+1)
+            elif (nextPos == 'E'):    
+                self.player.setPosX(self.player.getPosX()+1)
+        else:
+            print("Not a valid move.")
 
 
     def isValidMove(self, nextPos):
-    	validMove = true
-    	if (nextPos == 'N') and (self.player.getPosY() == 0):
-    		validMove = false
-    	elif (nextPos == 'W') and (self.player.getPosX == 0):
-    		validMove = false
-    	elif (nextPos == 'S') and (self.player.getPosY == self.neighborhood.getGridLength()):
-    		validMove = false
-    	elif (nextPos == 'E') and (self.player.getPosY == self.neighborhood.getGridLength()):	
-    		validMove = false
-    	return validMove
+        validMove = true
+        if (nextPos == 'N') and (self.player.getPosY() == 0):
+            validMove = false
+        elif (nextPos == 'W') and (self.player.getPosX == 0):
+            validMove = false
+        elif (nextPos == 'S') and (self.player.getPosY == self.neighborhood.getGridLength()):
+            validMove = false
+        elif (nextPos == 'E') and (self.player.getPosY == self.neighborhood.getGridLength()):    
+            validMove = false
+        return validMove
 
     def getInventory(self): 
-    	invSlot = 0
+        invSlot = 0
         print("\n--------------------------- Inventory -----------------------------------")
         for weapon in self.player.getInventory():
-        	wName = weapon.getName()
-        	wUses = weapon.getUses()
-        	wModif = weapon.getModif()
-        	print('\tInventory Slot: {num}, Weapon: {name}, Uses: {uses}, Modifier {mod}'
-        		.format(num=invSlot, name=wName, uses=wUses, mod = wModif))
+            wName = weapon.getName()
+            wUses = weapon.getUses()
+            wModif = weapon.getModif()
+            print('\tInventory Slot: {num}, Weapon: {name}, Uses: {uses}, Modifier {mod}'
+                .format(num=invSlot, name=wName, uses=wUses, mod = wModif))
             invSlot = invSLot + 1
     def playerHealth(self):
-    	print("\nPlayer Health: {}".format(player.getHealth()))
+        print("\nPlayer Health: {}".format(player.getHealth()))
 
     def totalMonsterCount(self):
         print("\nTotal Monsters Remaining: {}".format(neighborhood.getMonstersInHouses()))
+
+    def monstersAttack(self):
+    	tmpAttValue = 0
+    	for i in self.Neighborhood.getHouses()[self.player.getPosX()]
+    	[self.player.getPosY()].getMonsters():
+    		tmpAttValue = tmpAttValue + i.getAttack()
+
+    	self.player.setHealth(self.player.getHealth() - tmpAttValue)
+
+
+    	print("\nPlayer Attacked by the monster, yikes! Lost hp: {}".format(tmpAttValue))
+    
--- Home.py
+++ Home.py
@@ -4,69 +4,69 @@ from Observable import *
 from Observer import *
 
 class Home(Observable, Observer):
-	'''Home is an Observer of the monsters inside of it,
-	as well as observed by the neighborhood it lies in. 
-	A home can have up to 10 NPC's ranging from a person
-	to the four different monsters. The population of a
-	home is randomly generated and notifies when the
-	population is changed.'''
+    '''Home is an Observer of the monsters inside of it,
+    as well as observed by the neighborhood it lies in. 
+    A home can have up to 10 NPC's ranging from a person
+    to the four different monsters. The population of a
+    home is randomly generated and notifies when the
+    population is changed.'''
 
-	def __init__(self):
+    def __init__(self):
         Observable.__init__(self)
         Observer.__init__(self)
         self.monsters = popHome(self)
 
     def popHome(self):
-    	monsterList = []
-    	homePop = randint(0,10)
-    	possibleMon = ['Person', 'Vampire', 'Werewolves','Zombie', 'Ghouls']
-    	for numMon in range(0, homePop):
-    		randSelected = randint(0, 4)
-    		selected = possibleMon[randSelected]
-    		if select == 'Person':
-    			person = NPC.person() 
-    			super.add_Observer(person)
-    			monsterList.append(person)
-    		if select == 'Vampire':
-    			vampire = NPC.Vampires() 
-    			super.add_Observer(vampire)
-    			monsterList.append(vampire)
-    		if select == 'Werewolves':
-    			werewolves = NPC.Werewolves() 
-    			super.add_Observer(werewolves)
-    			monsterList.append(werewolves)
-    		if select == 'Zombie':
-    			zombie = NPC.Zombies() 
-    			super.add_Observer(zombie)
-    			monsterList.append(zombie)
-    		if select == 'Ghouls':
-    			ghouls = NPC.Ghouls() 
-    			super.add_Observer(ghouls)
-    			monsterList.append(ghouls)
-    	return monsterList
+        monsterList = []
+        homePop = random.randint(0,10)
+        possibleMon = ['Person', 'Vampire', 'Werewolves','Zombie', 'Ghouls']
+        for numMon in range(0, homePop):
+            randSelected = random.randint(0, 4)
+            selected = possibleMon[randSelected]
+            if select == 'Person':
+                person = NPC.person() 
+                super.add_Observer(person)
+                monsterList.append(person)
+            if select == 'Vampire':
+                vampire = NPC.Vampires() 
+                super.add_Observer(vampire)
+                monsterList.append(vampire)
+            if select == 'Werewolves':
+                werewolves = NPC.Werewolves() 
+                super.add_Observer(werewolves)
+                monsterList.append(werewolves)
+            if select == 'Zombie':
+                zombie = NPC.Zombies() 
+                super.add_Observer(zombie)
+                monsterList.append(zombie)
+            if select == 'Ghouls':
+                ghouls = NPC.Ghouls() 
+                super.add_Observer(ghouls)
+                monsterList.append(ghouls)
+        return monsterList
 
     def getMonsters(self):
-    	return self.monsters
+        return self.monsters
 
     def numMonster(self):
-    	total = 0
-    	for numMonster in self.monsters:
-    		if numMonster.getName() not in ['Person']:
-    			total = total + 1
-    	return total
+        total = 0
+        for numMonster in self.monsters:
+            if numMonster.getName() not in ['Person']:
+                total = total + 1
+        return total
 
     def newPerson(self, position):
-    	person = NPC.person()
-    	super.add_Observer(person)
-    	self.monsters.insert(position, person)
+        person = NPC.person()
+        super.add_Observer(person)
+        self.monsters.insert(position, person)
 
     def deleteMonster(self, position):
-    	del self.monsters[position]
-    	self.newPerson(self, position)
-    	self.update()
+        del self.monsters[position]
+        self.newPerson(self, position)
+        self.update()
 
     def update(self):
-    	super.notify()
+        super.notify()
 
 
 
