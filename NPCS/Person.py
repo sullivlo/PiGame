ff92224f71dd48ec2e3f5ffa0ed3cda3144f5f9e
Louis Sullivan <sullivlo@mail.gvsu.edu>
2018-03-22 13:14:33 -0400

    Commented Person

--- NPCS/Person.py
+++ NPCS/Person.py
@@ -1,6 +1,9 @@
 from random import *
 from NPCS.NPC import *
 
+'''Person is the observer class that creates the Person object
+init creates the persons object with health, attack, unaffected Candy,
+alt (string weapon), name, and is NPC alive'''
 class Person(NPC):
 	'''Persons are monsters/NPCs who have a health between of 100.
 	They are not harmed by the plater. Persons help you by giving 
@@ -14,14 +17,21 @@ class Person(NPC):
 		self.attack = self.genAttack()
 		self.unaffCandy = self.genUnaffCandy()
 		self.altCandy = self.genAltCandy()
-
-		
+	
+	"""Generates the Persons Health
+	return the persons health"""
 	def genHealth(self):
 		return 100
+	"""Generates the Persons attack
+	return the persons attack"""
 	def genAttack(self):
 		return -1
+	"""Generates the Persons altimate candy
+	return the persons altimate candy"""
 	def genAltCandy(self):
 		return []
+	"""Generates the Persons altimate candy
+	return the persons altimate candy"""
 	def genUnaffCandy(self):
 		return ['HersheyKisses','SourStraws','ChocolateBars','NerdBombs']
 
